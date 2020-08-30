662e3f2b300ecd561570d5f4f37a7967ed80370e
alifmuryp <alifmuryp@gmail.com>
2020-08-30 22:37:58 +0700

    nav salah dikit

---
+++ Navbar1/js/my.js
@@ -0,0 +1,5 @@
+const menuTogle = doccument.quaryselecktor ('.menu-togle input');
+nav doccument.quaryselecktor("nav ul");
+menuTogle.addEvenListener('click', function(){
+  navclassList.togle("slide");
+});
